6b4befb81cd4befce84975846db3f7d2fe584405
Ashish <ashish2017pandey@gmail.com>
2023-03-17 19:14:02 +0530

    inserting data

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -17,119 +17,153 @@ import { Router } from '@angular/router';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent implements OnInit {
   title = 'EMPLOYEE DATA FORM';
-  
-  step: any=1;
-  constructor(private builder:FormBuilder, private router: Router) { }
+
+  step: any = 1;
+  constructor(private builder: FormBuilder, private router: Router) { }
   ngOnInit() {
-      // this.loginUser()
+    // this.loginUser()
   }
- 
-  
-      // loginForm!:FormGroup;
 
-      //ngOnInit(){
-      //this.
-      loginForm = new FormGroup({
 
-          First_Name: new FormControl(''),
-          Middle_Name: new FormControl(''),
-          Last_Name: new FormControl(''),
-          date_of_joining: new FormControl(''),
-          Birthday_day: new FormControl(''),
-          Birthday_day_document: new FormControl(''),
-          nationality: new FormControl(''),
-          religion: new FormControl(''),
-          country: new FormControl(''),
-          Passport_Number: new FormControl(''),
-          Aadhar_Number: new FormControl(''),
-          Email_Id: new FormControl(''),
-          Mobile_Number: new FormControl(''),
-          Blood_group: new FormControl(''),
-          Radio01: new FormControl(''),
-          Radio02 :new FormControl(''),
-          Reason :new FormControl(''),
-          Name : new FormControl(''),
-          Title : new FormControl(''),
-          Department :new FormControl(''),
-          Address:new FormControl(''),
-          House_No1:new FormControl(''),
-          House_No2:new FormControl(''),
-          Street1:new FormControl(''),
-          Street2:new FormControl(''),
-          City1:new FormControl(''),
-          Pin_Code1:new FormControl(''),
-          City2:new FormControl(''),
-          Pin_Code2:new FormControl(''),
-          State1:new FormControl(''),
-          State2:new FormControl(''),
-          Telephone_No1:new FormControl(''),
-          Telephone_No2:new FormControl(''),
-          Name3:new FormControl(''),
-          Relationship:new FormControl(''),
-          Address2:new FormControl(''),
-          City3:new FormControl(''),
-          Phone2:new FormControl(''),
-          Email_ID3:new FormControl(''),
-          FatherName:new FormControl(''),
-          Occupation:new FormControl(''),
-          Dob:new FormControl(''),
-          MotherName:new FormControl(''),
-          Occupation2:new FormControl(''),
-          Dob2:new FormControl(''),
-          SpouseName:new FormControl(''),
-          Occupation3:new FormControl(''),
-          Dob3:new FormControl(''),
-          ChildName1:new FormControl(''),
-          Gender:new FormControl(''),
-          Dob4:new FormControl(''),
-          ChildName2:new FormControl(''),
-          Gender2:new FormControl(''),
-          Dob5:new FormControl(''),
-          From:new FormControl(''),
-          Organization:new FormControl(''),
-          Designation:new FormControl(''),
-          RM:new FormControl(''),
-          REASONL:new FormControl(''),
-          Qualification:new FormControl(''),
-          University_Board:new FormControl(''),
-          Specialization:new FormControl(''),
-          YR:new FormControl(""),
-          Grade_CGPA:new FormControl(''),
-          Name6:new FormControl(''),
-          Institute:new FormControl(''),
-          MY:new FormControl(''),
-          Name7:new FormControl(''),
-          Name8:new FormControl(''),
-          Designation2:new FormControl(''),
-          Designation3:new FormControl(''),
-          CN1:new FormControl(''),
-          CN2:new FormControl(''),
-          EM1:new FormControl(''),
-          EM2:new FormControl(''),
-          CLASS10:new FormControl(''),
-          CLASS12:new FormControl(''),
-          DEGREE1:new FormControl(''),
-          ACADEMIC:new FormControl(''),
-          PAN:new FormControl(''),
-          PAYSLIP:new FormControl(''),
-          RELIEVINGLETTER:new FormControl(''),
-          Experienceletter:new FormControl(''),
-          appointmentletter:new FormControl(''),
-          PHOTO:new FormControl(''),
-          Name_8:new FormControl(''),
-          Signature:new FormControl(''),
-          Date:new FormControl('')
-      })
-      
-      loginUser() {
-          console.warn(this.loginForm.value)
-          this.step=this.step+1;
-          
-      }
-  previous(){
-      this.step=this.step-1;
+  // loginForm!:FormGroup;
+
+  //ngOnInit(){
+  //this.
+  loginForm = new FormGroup({
+    Image: new FormControl(''),
+    First_Name: new FormControl(''),
+    Middle_Name: new FormControl(''),
+    Last_Name: new FormControl(''),
+    date_of_joining: new FormControl(''),
+    Birthday_day: new FormControl(''),
+    Birthday_day_document: new FormControl(''),
+    nationality: new FormControl(''),
+    religion: new FormControl(''),
+    country: new FormControl(''),
+    Passport_Number: new FormControl(''),
+    Aadhar_Number: new FormControl(''),
+    Email_Id: new FormControl(''),
+    Mobile_Number: new FormControl(''),
+    Blood_group: new FormControl(''),
+    Radio01: new FormControl(''),
+    Radio02: new FormControl(''),
+    Reason: new FormControl(''),
+    Name: new FormControl(''),
+    Title: new FormControl(''),
+    Department: new FormControl(''),
+    Address: new FormControl(''),
+    House_No1: new FormControl(''),
+    House_No2: new FormControl(''),
+    Street1: new FormControl(''),
+    Street2: new FormControl(''),
+    City1: new FormControl(''),
+    Pin_Code1: new FormControl(''),
+    City2: new FormControl(''),
+    Pin_Code2: new FormControl(''),
+    State1: new FormControl(''),
+    State2: new FormControl(''),
+    Telephone_No1: new FormControl(''),
+    Telephone_No2: new FormControl(''),
+    Name3: new FormControl(''),
+    Relationship: new FormControl(''),
+    Address2: new FormControl(''),
+    City3: new FormControl(''),
+    Phone2: new FormControl(''),
+    Email_ID3: new FormControl(''),
+    FatherName: new FormControl(''),
+    Occupation: new FormControl(''),
+    Dob: new FormControl(''),
+    MotherName: new FormControl(''),
+    Occupation2: new FormControl(''),
+    Dob2: new FormControl(''),
+    SpouseName: new FormControl(''),
+    Occupation3: new FormControl(''),
+    Dob3: new FormControl(''),
+    ChildName1: new FormControl(''),
+    Gender: new FormControl(''),
+    Dob4: new FormControl(''),
+    ChildName2: new FormControl(''),
+    Gender2: new FormControl(''),
+    Dob5: new FormControl(''),
+    From: new FormControl(''),
+    Organization: new FormControl(''),
+    Designation: new FormControl(''),
+    RM: new FormControl(''),
+    REASONL: new FormControl(''),
+    Qualification: new FormControl(''),
+    University_Board: new FormControl(''),
+    Specialization: new FormControl(''),
+    YR: new FormControl(""),
+    Grade_CGPA: new FormControl(''),
+    Qualification2: new FormControl(''),
+    University_Board2: new FormControl(''),
+    Specialization2: new FormControl(''),
+    YR2: new FormControl(""),
+    Grade_CGPA2: new FormControl(''),
+    Qualification3: new FormControl(''),
+    University_Board3: new FormControl(''),
+    Specialization3: new FormControl(''),
+    YR3: new FormControl(""),
+    Grade_CGPA3: new FormControl(''),
+    Qualification4: new FormControl(''),
+    University_Board4: new FormControl(''),
+    Specialization4: new FormControl(''),
+    YR4: new FormControl(""),
+    Grade_CGPA4: new FormControl(''),
+    Name6: new FormControl(''),
+    Institute: new FormControl(''),
+    MY: new FormControl(''),
+    UploadImage:new FormControl(''),
+    Name10: new FormControl(''),
+    Institute10: new FormControl(''),
+    MY10: new FormControl(''),
+    UploadImage10:new FormControl(''),
+    Name11: new FormControl(''),
+    Institute11: new FormControl(''),
+    MY11: new FormControl(''),
+    UploadImage11:new FormControl(''),
+    Name7: new FormControl(''),
+    Name8: new FormControl(''),
+    Designation2: new FormControl(''),
+    Designation3: new FormControl(''),
+    CN1: new FormControl(''),
+    CN2: new FormControl(''),
+    EM1: new FormControl(''),
+    EM2: new FormControl(''),
+    CLASS10: new FormControl(''),
+    UploadImageClass10:new FormControl(''),
+    CLASS12: new FormControl(''),
+    UploadImageClass12:new FormControl(''),
+    DEGREE1: new FormControl(''),
+    UploadImageDegree1:new FormControl(''),
+    ACADEMIC: new FormControl(''),
+    UploadImageACADEMIC:new FormControl(),
+    PAN: new FormControl(''),
+    UploadImagePAN:new FormControl(''),
+    PAYSLIP: new FormControl(''),
+    UploadImagePAYSLIP:new FormControl(''),
+    RELIEVINGLETTER: new FormControl(''),
+    UploadImageRELIEVINGLETTER:new FormControl(''),
+    Experienceletter: new FormControl(''),
+    UploadImageExperienceletter:new FormControl(''),
+    appointmentletter: new FormControl(''),
+    UploadImageappointmentletter:new FormControl(''),
+    PHOTO: new FormControl(''),
+    UploadImagePHOTO:new FormControl(''),
+    Name_8: new FormControl(''),
+    Signature: new FormControl(''),
+    Date: new FormControl('')
+  })
+
+  loginUser() {
+    console.warn(this.loginForm.value)
+    this.step = this.step + 1;
+
+  }
+  previous() {
+    this.step = this.step - 1;
   }
 
   submit() {
@@ -139,3 +173,4 @@ export class RegisterComponent implements OnInit{
     this.router.navigateByUrl('/');
   }
 }
+
